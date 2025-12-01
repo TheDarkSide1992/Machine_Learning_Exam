87104aac45630d461872208540921b9d19aa2ade
Jens <jenscp171@gmail.com>
2025-12-01 12:20:04 +0100

    created simple cli setup

--- exam_agent/__main__.py
+++ exam_agent/__main__.py
@@ -1,10 +1,11 @@
-from exam_agent.agents import *
+import argparse
+from exam_agent.agents import start
 
 
-def main(): #TODO Make it run the agents as exegesis requires
+def main(prompt:str=None, continuous_chat:bool=False, generate_recipe:bool=False):
     print("Starting up.....")
 
-    while True:
+    while continuous_chat:
         prompt = get_user_input()
         start(prompt=prompt)
 
@@ -18,4 +19,25 @@ def get_user_input() -> str:
     return prompt
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-P", "--prompt", type=str, help=f"Allows you to enter your prompt, before application start.")
+    parser.add_argument("-G", "--generate-recipe", action="store_true",
+                        help="Allows the agent to generate its own recipe. If flag is false the ai wil search for a recipe instead. this flag is of by default.")
+    parser.add_argument("-C", "--continuous-chat", action="store_true",
+                        help="Allows you to continuously prompt the agent without th application classing flag is true")
+    args = parser.parse_args()
+
+    prompt = None;
+    cc = False;
+    gr = False
+
+
+    if args.prompt is not None:
+        prompt = args.prompt
+    if args.generate_recipe:
+        gr = args.generate_recipe
+    if args.continuous_chat:
+        cc = args.continuous_chat
+
+    main(prompt=prompt, continuous_chat=cc, generate_recipe=gr)
