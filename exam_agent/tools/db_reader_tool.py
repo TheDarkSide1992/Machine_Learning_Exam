2c7246c596177832c98c82bf758462b62fbdfd76
Jens <jenscp171@gmail.com>
2025-12-03 11:35:38 +0100

    updated tool, to return a list of ToxicENtries

--- exam_agent/tools/db_reader_tool.py
+++ exam_agent/tools/db_reader_tool.py
@@ -2,6 +2,7 @@ import psycopg2
 from typing import TypedDict, Optional
 from exam_agent.config import DATABASE as DBCONFIG
 
+
 class ToxicEntry(TypedDict):
     sub_name: str
     sub_description: str
@@ -13,7 +14,7 @@ class ToxicEntry(TypedDict):
     is_carcinogenic: Optional[str]
     remarks_study: str
     riskunit: str
-    remarks :str
+    remarks: str
     assess: str
 
 
@@ -48,15 +49,44 @@ def search_database(search: str):
 
     cursor.execute(query=query)
 
-    record = cursor.fetchall()
+    records = cursor.fetchall()
 
-    # gets fields
-    field_names = list(ToxicEntry.__annotations__.keys())
+    """"
+    data_out = list([
+        (map(lambda x: {
+            "sub_name": x[0],
+            "sub_description": x[1],
+            "molecularformula": x[2],
+            "com_type": x[3],
+            "sub_op_class": x[4],
+            "is_mutagenic": x[5],
+            "is_genotoxic": x[6],
+            "is_carcinogenic": x[7],
+            "remarks_study": x[8],
+            "riskunit": x[9],
+            "remarks": x[10],
+            "assess": x[11],
+        }, record)) for record in records
+    ])"""
 
-    # maps to dictionarry
-    dict = dict(map(lambda field_names, record: (field_names, record), field_names, record))
 
-    return record
+    data_out:list = [ToxicEntry]
+    for record in records:
+        data_out.append({"sub_name": record[0],
+            "sub_description": record[1],
+            "molecularformula": record[2],
+            "com_type": record[3],
+            "sub_op_class": record[4],
+            "is_mutagenic": record[5],
+            "is_genotoxic": record[6],
+            "is_carcinogenic": record[7],
+            "remarks_study": record[8],
+            "riskunit": record[9],
+            "remarks": record[10],
+            "assess": record[11],
+            })
+
+    return data_out
 
 
 if __name__ == "__main__":
