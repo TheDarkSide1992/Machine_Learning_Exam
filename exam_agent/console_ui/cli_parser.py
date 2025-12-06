716f6d28fd08699b36f6e2a55281bc9bf234d144
Jens <jenscp171@gmail.com>
2025-12-06 13:12:23 +0100

    updated help page

--- exam_agent/console_ui/cli_parser.py
+++ exam_agent/console_ui/cli_parser.py
@@ -7,7 +7,7 @@ def get_parser_args():
 
     parser.add_argument("-P", "--prompt", type=str, help=f"Allows you to enter your prompt, before application start.")
     parser.add_argument("-C", "--continuous-chat", action="store_true",
-                        help="Allows you to continuously prompt the agent without th application closing, flag is true by default")
+                        help="Allows you to continuously prompt the agent without the application closing, flag is true by default")
     args = parser.parse_args()
 
     return args
